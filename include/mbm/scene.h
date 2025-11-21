9cbd131ed848495f7dfc8eddc03f345d79018031
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-21 17:26:17 -0500

    added scene module

--- include/mbm/scene.h
+++ include/mbm/scene.h
@@ -1,18 +1,15 @@
 #ifndef MBM_SCENE_H_INCLUDED
 #define MBM_SCENE_H_INCLUDED
 #include "mbm/abi.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
 #include <stddef.h>
 
-// `struct scene` is an opaque data structure
-struct scene;
-
-// define `Scene` as an alias for `struct scene`
-typedef struct scene Scene;
-
-// forward function declarations
 MBM_ABI void scene_deinit (void);
-MBM_ABI void scene_delete (struct scene ** scene);
-MBM_ABI void scene_init (void);
-MBM_ABI struct scene * scene_new (void);
+MBM_ABI void scene_delete (void);
+MBM_ABI void scene_draw (void);
+MBM_ABI void scene_init (SDL_Window * window, SDL_Renderer * renderer);
+MBM_ABI void scene_new (void);
+MBM_ABI void scene_update (void);
 
 #endif
--- src/mbm/scene.c
+++ src/mbm/scene.c
@@ -1,6 +1,7 @@
 #include "mbm/scene.h"
 #include "SDL3/SDL_rect.h"
 #include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,11 +9,14 @@
 
 // declare properties of `struct scene`
 struct scene {
+    SDL_Color color;
     float ratio;
     struct {
         SDL_FRect sim;
-        SDL_Rect tgt;
+        SDL_FRect tgt;
     } rects;
+    SDL_Renderer * renderer;
+    SDL_Window * window;
 };
 
 // define pointer to singleton instance of `struct scene`
@@ -20,49 +24,67 @@ static struct scene * scene = nullptr;
 
 void scene_deinit (void) {}
 
-void scene_delete (struct scene ** scene) {
-    free(*scene);
-    *scene = nullptr;
+void scene_delete (void) {
+    free(scene);
+    scene = nullptr;
 }
 
 SDL_FRect scene_get_rect_scene (void) {
     return scene->rects.sim;
 }
 
-void scene_draw (SDL_Renderer * renderer) {
-    SDL_RenderRect(renderer, &scene->rects.sim);
+void scene_draw (void) {
+    SDL_SetRenderDrawColor(scene->renderer, scene->color.r, scene->color.g, scene->color.b, scene->color.a);
+    SDL_RenderFillRect(scene->renderer, &scene->rects.tgt);
 }
 
 void scene_update (void) {
-    scene->rects.tgt = (SDL_Rect) {
-        .h = 20,
-        .w = 100,
+    int w = -1;
+    int h = -1;
+    SDL_GetWindowSize(scene->window, &w, &h);
+
+    //int ratio = h / w;
+    //if (ratio < scene->ratio) {
+    //} else {
+    //}
+
+    scene->rects.tgt = (SDL_FRect) {
+        .h = h - 20,
+        .w = w - 20,
         .x = 10,
-        .y = 20,
+        .y = 10,
     };
 }
 
-void scene_init (void) {
+void scene_init (SDL_Window * window, SDL_Renderer * renderer) {
+    scene->color = (SDL_Color) {
+        .r = 128,
+        .g = 0,
+        .b = 128,
+        .a = SDL_ALPHA_OPAQUE,
+    };
     int h = 720;
     int w = 1024;
+    scene->ratio = (float) w / h;
     scene->rects.sim = (SDL_FRect) {
         .h = h,
         .w = w,
         .x = 0,
         .y = 0,
     };
-    scene->ratio = (float) w / h;
+    scene->renderer = renderer;
+    scene->window = window;
 }
 
-struct scene * scene_new (void) {
+void scene_new (void) {
     if (scene != nullptr) {
-        // return a pointer to existing singleton instance of scene
-        return scene;
+        // memory has already been allocated for `scene`, and `scene` is singleton
+        return;
     }
     scene = (struct scene *) calloc(1, sizeof(struct scene));
     if (scene == nullptr) {
         fprintf(stderr, "ERROR allocating dynamic memory for struct scene, aborting.\n");
         exit(1);
     }
-    return scene;
+    return;
 }
