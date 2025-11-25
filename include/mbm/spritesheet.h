1ee9fcebe132780229960d5278951695f9171c58
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-25 11:38:00 -0500

    added renderer argument to .init(); added getter methods

--- include/mbm/spritesheet.h
+++ include/mbm/spritesheet.h
@@ -1,8 +1,8 @@
 #ifndef MBM_SPRITESHEET_H_INCLUDED
 #define MBM_SPRITESHEET_H_INCLUDED
 #include "mbm/abi.h"
-#include "SDL3/SDL_surface.h"
-#include "SDL3/SDL_rect.h"
+#include "SDL3/SDL_rect.h"        // type `SDL_Rect`
+#include <SDL3/SDL_render.h>      // type `SDL_Renderer`, `SDL_Texture`
 
 // `struct spritesheet` is an opaque data structure
 struct spritesheet;
@@ -12,11 +12,16 @@ typedef struct spritesheet Spritesheet;
 
 MBM_ABI void spritesheet_delete (struct spritesheet ** self);
 MBM_ABI char * spritesheet_get_path (struct spritesheet * self);
-MBM_ABI SDL_Rect spritesheet_get_rect_balloon_orange (struct spritesheet * self);
-MBM_ABI SDL_Rect spritesheet_get_rect_balloon_red (struct spritesheet * self);
-MBM_ABI SDL_Rect spritesheet_get_rect_balloon_yellow (struct spritesheet * self);
-MBM_ABI SDL_Surface * spritesheet_get_surface (struct spritesheet * self);
-MBM_ABI void spritesheet_init (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_balloon_orange (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_balloon_red (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_balloon_yellow (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_barrel (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_bullet (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_collision (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_flash (struct spritesheet * self);
+MBM_ABI SDL_FRect * spritesheet_get_moon (struct spritesheet * self);
+MBM_ABI SDL_Texture * spritesheet_get_texture (struct spritesheet * self);
+MBM_ABI void spritesheet_init (struct spritesheet * self, SDL_Renderer * renderer);
 MBM_ABI struct spritesheet * spritesheet_new (void);
 
 #endif
