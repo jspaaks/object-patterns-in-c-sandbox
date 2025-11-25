e63b802152137c7f8ef7c87465028ee684b0e8d6
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-25 11:35:18 -0500

    moved renderer argument from .init() to .draw()

--- include/mbm/ground.h
+++ include/mbm/ground.h
@@ -1,7 +1,7 @@
 #ifndef MBM_GROUND_H_INCLUDED
 #define MBM_GROUND_H_INCLUDED
 #include "mbm/abi.h"
-#include "mbm/scene.h"
+#include "mbm/scene.h"            // type `struct scene`
 #include <SDL3/SDL_render.h>      // type `SDL_Renderer`
 
 // `struct ground` is an opaque data structure
@@ -11,8 +11,8 @@ struct ground;
 typedef struct ground Ground;
 
 MBM_ABI void ground_delete (struct ground ** self);
-MBM_ABI void ground_draw (struct ground * self);
-MBM_ABI void ground_init (struct ground * self, SDL_Renderer * renderer);
+MBM_ABI void ground_draw (struct ground * self, SDL_Renderer * renderer);
+MBM_ABI void ground_init (struct ground * self);
 MBM_ABI struct ground * ground_new (void);
 MBM_ABI void ground_update (struct ground * self, struct scene * scene);
 
