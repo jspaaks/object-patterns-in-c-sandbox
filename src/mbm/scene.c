ac87711b71099e79ae71d27a1117d197e6cc33a1
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-25 08:39:54 -0500

    updated scene to help facilitate scaling and placement of subsequent objects

--- src/mbm/scene.c
+++ src/mbm/scene.c
@@ -10,6 +10,7 @@
 struct scene {
     SDL_Color color;
     float ratio;
+    float scale;
     SDL_FRect sim;
     SDL_FRect tgt;
     SDL_Renderer * renderer;
@@ -24,47 +25,26 @@ void scene_delete (struct scene ** self) {
     *self = nullptr;
 }
 
-SDL_FRect scene_get_rect_scene (struct scene * self) {
-    return self->sim;
-}
-
 void scene_draw (struct scene * self) {
     SDL_SetRenderDrawColor(self->renderer, self->color.r, self->color.g, self->color.b, self->color.a);
     SDL_RenderFillRect(self->renderer, &self->tgt);
 }
 
-void scene_update (struct scene * self) {
-    int h = -1;
-    int w = -1;
-    SDL_GetWindowSize(self->window, &w, &h);
+float scene_get_tgt_x (struct scene * self) {
+    return self->tgt.x;
+}
 
-    float ratio = (float) w / h;
-    if (ratio < self->ratio) {
-        // too tall
-        float scale = (float) w / self->sim.w;
-        float h0 = scale * self->sim.w / self->ratio;
-        self->tgt = (SDL_FRect) {
-            .h = h0,
-            .w = scale * self->sim.w,
-            .x = 0,
-            .y = ((float) h - h0) / 2,
-        };
-    } else {
-        // too wide
-        float scale = (float) h / self->sim.h;
-        float w0 = scale * self->sim.h * self->ratio;
-        self->tgt = (SDL_FRect) {
-            .h = scale * self->sim.h,
-            .w = w0,
-            .x = ((float) w - w0) / 2,
-            .y = 0,
-        };
-    }
+float scene_get_tgt_y (struct scene * self) {
+    return self->tgt.y;
+}
+
+float scene_get_scale (struct scene * self) {
+    return self->scale;
 }
 
 void scene_init (struct scene * self, SDL_Window * window, SDL_Renderer * renderer) {
-    int h = 720;
-    int w = 1280;
+    const int height = 720;
+    const int width = 1280;
     *self = (struct scene) {
         .color = (SDL_Color) {
             .r = 0,
@@ -72,17 +52,18 @@ void scene_init (struct scene * self, SDL_Window * window, SDL_Renderer * render
             .b = 43,
             .a = SDL_ALPHA_OPAQUE,
         },
-        .ratio = (float) w / h,
+        .ratio = (float) width / height,
         .renderer = renderer,
+        .scale = 1.0f,
         .sim = (SDL_FRect) {
-            .h = h,
-            .w = w,
+            .h = height,
+            .w = width,
             .x = 0,
             .y = 0,
         },
         .tgt = (SDL_FRect) {
-            .h = h,
-            .w = w,
+            .h = height,
+            .w = width,
             .x = 0,
             .y = 0,
         },
@@ -101,4 +82,33 @@ struct scene * scene_new (void) {
         exit(1);
     }
     return singleton;
+}
+
+void scene_update (struct scene * self) {
+    int h = -1;
+    int w = -1;
+    SDL_GetWindowSize(self->window, &w, &h);
+
+    float ratio = (float) w / h;
+    if (ratio < self->ratio) {
+        // too tall
+        self->scale = (float) w / self->sim.w;
+        float h0 = self->scale * self->sim.w / self->ratio;
+        self->tgt = (SDL_FRect) {
+            .h = h0,
+            .w = self->scale * self->sim.w,
+            .x = 0,
+            .y = ((float) h - h0) / 2,
+        };
+    } else {
+        // too wide
+        self->scale = (float) h / self->sim.h;
+        float w0 = self->scale * self->sim.h * self->ratio;
+        self->tgt = (SDL_FRect) {
+            .h = self->scale * self->sim.h,
+            .w = w0,
+            .x = ((float) w - w0) / 2,
+            .y = 0,
+        };
+    }
 }
