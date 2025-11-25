42ed2cf5b9ecf69adb7ef8a1cba0741e7fce0ad7
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-25 11:48:53 -0500

    added renderer as argument to drawing functions; added window as argument to updating functions; added creating, initializing, updating, drawing, deleting of moon object

--- src/mbm/game.c
+++ src/mbm/game.c
@@ -1,6 +1,7 @@
 #include "mbm/background.h"       // type `Background` and associated functions
 #include "mbm/game.h"             // type `Game` and associated functions
 #include "mbm/ground.h"           // type `Ground` and associated functions
+#include "mbm/moon.h"             // type `Moon` and associated functions
 #include "mbm/scene.h"            // type `Scene` and associated functions
 #include "mbm/spritesheet.h"      // type `Spritesheet` and associated functions
 #include <SDL3/SDL_events.h>      // type `SDL_Event`
@@ -16,9 +17,9 @@ typedef enum {
     MBM_GAME_STATE_LEN,
 } State;
 
-typedef void (*DrawFunction)(struct game *);
+typedef void (*DrawFunction)(struct game * game, SDL_Renderer * renderer);
 typedef SDL_AppResult (*HandleEventFunction)(SDL_Event * event);
-typedef void (*UpdateFunction)(struct game *);
+typedef void (*UpdateFunction)(struct game * game, SDL_Window * window);
 
 // declare properties of `struct game`
 struct game {
@@ -26,6 +27,7 @@ struct game {
     DrawFunction draw_functions[MBM_GAME_STATE_LEN];
     HandleEventFunction handle_event_functions[MBM_GAME_STATE_LEN];
     Ground * ground;
+    Moon * moon;
     State state;
     Scene * scene;
     Spritesheet * spritesheet;
@@ -36,16 +38,17 @@ struct game {
 static struct game * singleton = nullptr;
 
 // forward function declarations
-static void game_draw_lobby (struct game * self);
-static void game_draw_playing (struct game * self);
+static void game_draw_lobby (struct game * self, SDL_Renderer * renderer);
+static void game_draw_playing (struct game * self, SDL_Renderer * renderer);
 static SDL_AppResult game_handle_event_lobby (SDL_Event * event);
 static SDL_AppResult game_handle_event_playing (SDL_Event * event);
-static void game_update_lobby (struct game * self);
-static void game_update_playing (struct game * self);
+static void game_update_lobby (struct game * self, SDL_Window * window);
+static void game_update_playing (struct game * self, SDL_Window * window);
 
 void game_delete (struct game ** self) {
 
-    // delete objects
+    // delegate freeing dynamically allocated memory to the respective objects
+    moon_delete(&(*self)->moon);
     ground_delete(&(*self)->ground);
     scene_delete(&(*self)->scene);
     background_delete(&(*self)->background);
@@ -56,19 +59,22 @@ void game_delete (struct game ** self) {
     *self = nullptr;
 }
 
-void game_draw (struct game * self) {
-    self->draw_functions[self->state](self);
+void game_draw (struct game * self, SDL_Renderer * renderer) {
+    self->draw_functions[self->state](self, renderer);
 }
 
-static void game_draw_lobby (struct game * self) {
-    background_draw(self->background);
-    scene_draw(self->scene);
-    ground_draw(self->ground);
+static void game_draw_lobby (struct game * self, SDL_Renderer * renderer) {
+    background_draw(self->background, renderer);
+    scene_draw(self->scene, renderer);
+    ground_draw(self->ground, renderer);
+    moon_draw(self->moon, self->spritesheet, renderer);
 }
 
-static void game_draw_playing (struct game * self) {
-    background_draw(self->background);
-    scene_draw(self->scene);
+static void game_draw_playing (struct game * self, SDL_Renderer * renderer) {
+    background_draw(self->background, renderer);
+    scene_draw(self->scene, renderer);
+    ground_draw(self->ground, renderer);
+    moon_draw(self->moon, self->spritesheet, renderer);
 }
 
 SDL_AppResult game_handle_event (struct game * self, SDL_Event * event) {
@@ -99,7 +105,7 @@ static SDL_AppResult game_handle_event_playing (SDL_Event * event) {
     return SDL_APP_CONTINUE;
 }
 
-void game_init (struct game * self, SDL_Window * window, SDL_Renderer * renderer) {
+void game_init (struct game * self, SDL_Renderer * renderer) {
 
     // empty-initialize the singleton instance of `struct game`
     *self = (struct game) {};
@@ -121,19 +127,23 @@ void game_init (struct game * self, SDL_Window * window, SDL_Renderer * renderer
 
     // initialize the spritesheet
     self->spritesheet = spritesheet_new();
-    spritesheet_init(self->spritesheet);
+    spritesheet_init(self->spritesheet, renderer);
 
     // initialize the scene
     self->scene = scene_new();
-    scene_init(self->scene, window, renderer);
+    scene_init(self->scene);
 
     // initialize the background
     self->background = background_new();
-    background_init(self->background, renderer);
+    background_init(self->background);
 
     // initialize the ground
     self->ground = ground_new();
-    ground_init(self->ground, renderer);
+    ground_init(self->ground);
+
+    // initialize the moon
+    self->moon = moon_new();
+    moon_init(self->moon, self->scene);
 }
 
 struct game * game_new (void) {
@@ -149,18 +159,20 @@ struct game * game_new (void) {
     return singleton;
 }
 
-void game_update (struct game * self) {
-    self->update_functions[self->state](self);
+void game_update (struct game * self, SDL_Window * window) {
+    self->update_functions[self->state](self, window);
 }
 
-static void game_update_lobby (struct game * self) {
+static void game_update_lobby (struct game * self, SDL_Window * window) {
     background_update(self->background);
-    scene_update(self->scene);
+    scene_update(self->scene, window);
     ground_update(self->ground, self->scene);
+    moon_update(self->moon, self->scene);
 }
 
-static void game_update_playing (struct game * self) {
+static void game_update_playing (struct game * self, SDL_Window * window) {
     background_update(self->background);
-    scene_update(self->scene);
+    scene_update(self->scene, window);
     ground_update(self->ground, self->scene);
+    moon_update(self->moon, self->scene);
 }
