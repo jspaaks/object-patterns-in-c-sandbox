36a5c593b3d6a43f33fa2403e5277a6d7e0eed11
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-25 11:56:33 -0500

    add resource cleanup for spritesheet; removed surface as a member of internal state; added .texture as a member of internal state; added renderer as an argument to .init(); changed return type of getter methods from SDL_Rect to SDL_FRect *

--- src/mbm/spritesheet.c
+++ src/mbm/spritesheet.c
@@ -2,6 +2,7 @@
 #include "SDL3/SDL_filesystem.h"
 #include "SDL3/SDL_log.h"
 #include "SDL3/SDL_rect.h"
+#include "SDL3/SDL_render.h"
 #include "SDL3/SDL_stdinc.h"
 #include "SDL3/SDL_surface.h"
 #include <stddef.h>
@@ -10,26 +11,34 @@
 
 // declare properties of `struct spritesheet`
 struct spritesheet {
-    char * path;
     struct {
-        SDL_Rect orange;
-        SDL_Rect red;
-        SDL_Rect yellow;
+        SDL_FRect orange;
+        SDL_FRect red;
+        SDL_FRect yellow;
     } balloon;
-    SDL_Rect barrel;
-    SDL_Rect bullet;
-    SDL_Rect collision;
-    SDL_Rect flash;
-    SDL_Rect moon;
-    SDL_Rect turret;
-    SDL_Surface * surface;
+    SDL_FRect barrel;
+    SDL_FRect bullet;
+    SDL_FRect collision;
+    SDL_FRect flash;
+    SDL_FRect moon;
+    char * path;
+    SDL_Texture * texture;
+    SDL_FRect turret;
 };
 
 // define pointer to singleton instance of `struct singleton`
 static struct spritesheet * singleton = nullptr;
 
 void spritesheet_delete (struct spritesheet ** self) {
+    // free dynamically allocated memory used by .texture
+    SDL_DestroyTexture((*self)->texture);
+    (*self)->texture = nullptr;
+
+    // free dynamically allocated memory used by .path
     SDL_free((*self)->path);
+    (*self)->path = nullptr;
+
+    // free own resources
     free(*self);
     *self = nullptr;
 }
@@ -38,84 +47,111 @@ char * spritesheet_get_path (struct spritesheet * self) {
     return self->path;
 }
 
-SDL_Rect spritesheet_get_rect_balloon_orange (struct spritesheet * self) {
-    return self->balloon.orange;
+SDL_FRect * spritesheet_get_balloon_orange (struct spritesheet * self) {
+    return &self->balloon.orange;
 }
 
-SDL_Rect spritesheet_get_rect_balloon_red (struct spritesheet * self) {
-    return self->balloon.red;
+SDL_FRect * spritesheet_get_balloon_red (struct spritesheet * self) {
+    return &self->balloon.red;
 }
 
-SDL_Rect spritesheet_get_rect_balloon_yellow (struct spritesheet * self) {
-    return self->balloon.yellow;
+SDL_FRect * spritesheet_get_balloon_yellow (struct spritesheet * self) {
+    return &self->balloon.yellow;
 }
 
-SDL_Surface * spritesheet_get_surface (struct spritesheet * self) {
-    return self->surface;
+SDL_FRect * spritesheet_get_barrel (struct spritesheet * self) {
+    return &self->barrel;
 }
 
-void spritesheet_init (struct spritesheet * self) {
-    self->balloon.orange = (SDL_Rect) {
-        .h = 12,
-        .w = 9,
-        .x = 184,
-        .y = 1,
+SDL_FRect * spritesheet_get_bullet (struct spritesheet * self) {
+    return &self->bullet;
+}
+
+SDL_FRect * spritesheet_get_collision (struct spritesheet * self) {
+    return &self->collision;
+}
+
+SDL_FRect * spritesheet_get_flash (struct spritesheet * self) {
+    return &self->flash;
+}
+
+SDL_FRect * spritesheet_get_moon (struct spritesheet * self) {
+    return &self->moon;
+}
+
+SDL_Texture * spritesheet_get_texture (struct spritesheet * self) {
+    return self->texture;
+}
+
+void spritesheet_init (struct spritesheet * self, SDL_Renderer * renderer) {
+    self->balloon.orange = (SDL_FRect) {
+        .h = 12.0,
+        .w = 9.0,
+        .x = 184.0,
+        .y = 1.0,
     };
-    self->balloon.red = (SDL_Rect) {
-        .h = 8,
-        .w = 6,
-        .x = 184,
-        .y = 20,
+    self->balloon.red = (SDL_FRect) {
+        .h = 8.0,
+        .w = 6.0,
+        .x = 184.0,
+        .y = 20.0,
     };
-    self->balloon.yellow = (SDL_Rect) {
-        .h = 16,
-        .w = 12,
-        .x = 166,
-        .y = 1,
+    self->balloon.yellow = (SDL_FRect) {
+        .h = 16.0,
+        .w = 12.0,
+        .x = 166.0,
+        .y = 1.0,
     };
-    self->barrel = (SDL_Rect) {
-        .h = 11,
-        .w = 55,
-        .x = 4,
-        .y = 68,
+    self->barrel = (SDL_FRect) {
+        .h = 11.0,
+        .w = 55.0,
+        .x = 4.0,
+        .y = 68.0,
     };
-    self->bullet = (SDL_Rect) {
-        .x = 188,
-        .y = 38,
-        .w = 5,
-        .h = 5
+    self->bullet = (SDL_FRect) {
+        .x = 188.0,
+        .y = 38.0,
+        .w = 5.0,
+        .h = 5.0
     };
-    self->collision = (SDL_Rect) {
-         .x = 172,
-         .y = 38,
-         .w = 3,
-         .h = 3,
+    self->collision = (SDL_FRect) {
+         .x = 172.0,
+         .y = 38.0,
+         .w = 3.0,
+         .h = 3.0,
     };
-    self->flash = (SDL_Rect) {
-         .x = 166,
-         .y = 63,
-         .w = 30,
-         .h = 21,
+    self->flash = (SDL_FRect) {
+         .x = 166.0,
+         .y = 63.0,
+         .w = 30.0,
+         .h = 21.0,
     };
-    self->moon = (SDL_Rect) {
-        .x = 75,
-        .y = 1,
-        .w = 90,
-        .h = 90,
+    self->moon = (SDL_FRect) {
+        .x = 75.0,
+        .y = 1.0,
+        .w = 90.0,
+        .h = 90.0,
     };
-    self->turret = (SDL_Rect) {
-       .h = 47,
-       .w = 69,
-       .x = 4,
-       .y = 1,
+    self->turret = (SDL_FRect) {
+       .h = 47.0,
+       .w = 69.0,
+       .x = 4.0,
+       .y = 1.0,
     };
     const char * relpath = "../share/mbm/assets/images/sprites.bmp";
     SDL_asprintf(&self->path, "%s%s", SDL_GetBasePath(), relpath);
-    self->surface = SDL_LoadBMP(self->path);
-    if (self->surface == nullptr) {
+    SDL_Surface * surface = SDL_LoadBMP(self->path);
+    if (surface == nullptr) {
         SDL_Log("Couldn't load sprites bitmap: %s", SDL_GetError());
         exit(1);
     }
+    self->texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (self->texture == nullptr) {
+        SDL_Log("Couldn't create static texture: %s", SDL_GetError());
+        exit(1);
+    }
+    SDL_DestroySurface(surface);
+    surface = nullptr;
 }
 
 struct spritesheet * spritesheet_new (void) {
