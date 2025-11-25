4edab0c37e5f18375734e9a370516b7f84f276b8
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-11-25 11:34:48 -0500

    moved renderer argument from .init() to .draw()

--- include/mbm/background.h
+++ include/mbm/background.h
@@ -1,7 +1,7 @@
 #ifndef MBM_BACKGROUND_H_INCLUDED
 #define MBM_BACKGROUND_H_INCLUDED
 #include "mbm/abi.h"
-#include "SDL3/SDL_render.h"
+#include <SDL3/SDL_render.h>      // type `SDL_Renderer`
 
 // `struct background` is an opaque data structure
 struct background;
@@ -10,8 +10,8 @@ struct background;
 typedef struct background Background;
 
 MBM_ABI void background_delete (struct background ** self);
-MBM_ABI void background_draw (struct background * self);
-MBM_ABI void background_init (struct background * self, SDL_Renderer * renderer);
+MBM_ABI void background_draw (struct background * self, SDL_Renderer * renderer);
+MBM_ABI void background_init (struct background * self);
 MBM_ABI struct background * background_new (void);
 MBM_ABI void background_update (struct background * self);
 
