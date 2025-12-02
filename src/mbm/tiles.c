8cdba5fbf70e5c2264f0a56c3d6b345ab61915b7
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-12-02 09:39:43 -0500

    now able to scroll the tiles

--- src/mbm/tiles.c
+++ src/mbm/tiles.c
@@ -8,6 +8,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/param.h>
 
 typedef enum {
     TILE_TYPE_AIR = 0,
@@ -23,25 +24,66 @@ struct tiles {
     SDL_FRect srcs[TILE_TYPE_COUNT];
     int tile_height;
     TileType ** tile_types;
-    TileType * tile_types_mem;
     int tile_width;
+    SDL_FRect view;
 };
+
+// forward declaration of static functions
+static SDL_Texture * load_tile_index (const char * relpath, SDL_Renderer * renderer);
+static void allocate_tiles (int nrows, int ncols, TileType *** tile_types);
 
 // define pointer to singleton instance of `struct tiles`
 static struct tiles * singleton = nullptr;
+
+static void allocate_tiles (const int nrows, const int ncols, TileType *** tile_types) {
+    TileType * mem = (TileType *) calloc(nrows * ncols, sizeof(TileType));
+    if (mem == nullptr) {
+        SDL_Log("Error allocating dynamic memory for contiguous memory for tile types, aborting.\n");
+        exit(1);
+    }
+    *tile_types = (TileType **) calloc(nrows, sizeof(TileType *));
+    if (*tile_types == nullptr) {
+        SDL_Log("Error allocating dynamic memory for memory for tile types, aborting.\n");
+        exit(1);
+    }
+    for (int irow = 0; irow < nrows; ++irow) {
+        (*tile_types)[irow] = &mem[irow * ncols];
+    }
+}
+
+static SDL_Texture * load_tile_index (const char * relpath, SDL_Renderer * renderer) {
+    char * path = nullptr;
+    SDL_asprintf(&path, "%s%s", SDL_GetBasePath(), relpath);
+    SDL_Surface * surface = SDL_LoadBMP(path);
+    if (surface == nullptr) {
+        SDL_Log("Couldn't load tiles bitmap: %s", SDL_GetError());
+        exit(1);
+    }
+    SDL_Texture * index = SDL_CreateTextureFromSurface(renderer, surface);
+    if (index == nullptr) {
+        SDL_Log("Couldn't create index: %s", SDL_GetError());
+        exit(1);
+    }
+    // free dynamically allocated memory used by surface and by path
+    SDL_DestroySurface(surface);
+    surface = nullptr;
+    SDL_free(path);
+    path = nullptr;
+    return index;
+}
 
 void tiles_delete (struct tiles ** self) {
     // free dynamically allocated memory used by .index
     SDL_DestroyTexture((*self)->index);
     (*self)->index = nullptr;
 
+    // free memory holding the contiguous tile type data
+    free((*self)->tile_types[0]);
+    (*self)->tile_types[0] = nullptr;
+
     // free memory holding the row/column tile type data
     free((*self)->tile_types);
     (*self)->tile_types = nullptr;
-
-    // free contiguous memory holding the tile type data
-    free((*self)->tile_types_mem);
-    (*self)->tile_types_mem = nullptr;
 
     // free own resources
     free(*self);
@@ -49,14 +91,16 @@ void tiles_delete (struct tiles ** self) {
 }
 
 void tiles_draw (struct tiles * self, SDL_Renderer * renderer) {
+    int icol_s = (int) (self->view.x) / self->tile_width;
+    int icol_e = MIN((int) (self->view.x + self->view.w) / self->tile_width + 1, self->ncols);
     for (int irow = 0; irow < self->nrows; irow++) {
-        for (int icol = 0; icol < self->ncols; icol++) {
+        for (int icol = icol_s; icol < icol_e; icol++) {
             TileType t = self->tile_types[irow][icol];
             SDL_FRect src = self->srcs[t];
             SDL_FRect wld = {
                 .h = (float) (self->tile_height),
                 .w = (float) (self->tile_width),
-                .x = (float) (icol * self->tile_width) - 16,
+                .x = (float) (icol * self->tile_width) - self->view.x,
                 .y = (float) (irow * self->tile_height),
             };
             SDL_RenderTexture(renderer, self->index, &src, &wld);
@@ -65,63 +109,60 @@ void tiles_draw (struct tiles * self, SDL_Renderer * renderer) {
 }
 
 void tiles_init (struct tiles * self, SDL_Renderer * renderer) {
-    {
-        self->nrows = 12;
-        self->ncols = 30;
-        self->tile_height = 32;
-        self->tile_width = 32;
-    }
-    {
-        const char * relpath = "../share/mbm/assets/images/tiles.bmp";
-        char * path = nullptr;
-        SDL_asprintf(&path, "%s%s", SDL_GetBasePath(), relpath);
-        SDL_Surface * surface = SDL_LoadBMP(path);
-        if (surface == nullptr) {
-            SDL_Log("Couldn't load tiles bitmap: %s", SDL_GetError());
-            exit(1);
-        }
-        self->index = SDL_CreateTextureFromSurface(renderer, surface);
-        if (self->index == nullptr) {
-            SDL_Log("Couldn't create index: %s", SDL_GetError());
-            exit(1);
-        }
+    SDL_Texture * index = nullptr;
+    int ncols = 100;
+    int nrows = 12;
+    int tile_height = 32;
+    TileType ** tile_types = nullptr;
+    int tile_width = 32;
 
-        // free dynamically allocated memory used by surface and by path
-        SDL_DestroySurface(surface);
-        surface = nullptr;
-        SDL_free(path);
-        path = nullptr;
-    }
+    // load the tile index into a texture 
+    index = load_tile_index("../share/mbm/assets/images/tiles.bmp", renderer);
+
+    // allocate memory for accessing the tiles consecutively and by row/col; 
+    allocate_tiles(nrows, ncols, &tile_types);
+
+    // initialize a tile pattern
     {
-        self->srcs[TILE_TYPE_AIR] = (SDL_FRect) {
-            .h = (float) (self->tile_height - 2),
-            .w = (float) (self->tile_width - 2),
-            .x = 1.0f,
-            .y = 1.0f,
-        };
-        self->srcs[TILE_TYPE_GROUND] = (SDL_FRect) {
-            .h = (float) (self->tile_height - 2),
-            .w = (float) (self->tile_width - 2),
-            .x = 32.0f + 10.0f + 1.0f,
-            .y = 1.0f,
-        };
-    }
-    {
-        self->tile_types_mem = (TileType *) calloc(self->nrows * self->ncols, sizeof(TileType));
-        self->tile_types = (TileType **) calloc(self->nrows, sizeof(TileType *));
-        for (int irow = 0; irow < self->nrows; irow++) {
-            self->tile_types[irow] = &self->tile_types_mem[irow * self->ncols];
-        }
-        for (int irow = 0; irow < self->nrows; irow++) {
-            for (int icol = 0; icol < self->ncols; icol++) {
-                if (irow == icol % self->nrows) {
-                    self->tile_types[irow][icol] = TILE_TYPE_GROUND;
+        for (int irow = 0; irow < nrows; irow++) {
+            for (int icol = 0; icol < ncols; icol++) {
+                if (irow == icol % nrows) {
+                    tile_types[irow][icol] = TILE_TYPE_GROUND;
                 } else {
-                    self->tile_types[irow][icol] = TILE_TYPE_AIR;
+                    tile_types[irow][icol] = TILE_TYPE_AIR;
                 }
             }
         }
     }
+
+    *self = (struct tiles) {
+        .index = index,
+        .ncols = ncols,
+        .nrows = nrows,
+        .srcs = {
+            [TILE_TYPE_AIR] = (SDL_FRect) {
+                .h = (float) (tile_height - 2),
+                .w = (float) (tile_width - 2),
+                .x = 1.0f,
+                .y = 1.0f,
+            },
+            [TILE_TYPE_GROUND] = (SDL_FRect) {
+                .h = (float) (tile_height - 2),
+                .w = (float) (tile_width - 2),
+                .x = 32.0f + 10.0f + 1.0f,
+                .y = 1.0f,
+            },
+        },
+        .tile_height = tile_height,
+        .tile_types = tile_types,
+        .tile_width = tile_width,
+        .view = (SDL_FRect) {
+            .h = 12 * tile_height,
+            .w = 20 * tile_height,
+            .x = 0,
+            .y = 0,
+        },
+    };
 }
 
 struct tiles * tiles_new (void) {
@@ -137,4 +178,8 @@ struct tiles * tiles_new (void) {
     return singleton;
 }
 
-void tiles_update (struct tiles *) {}
+void tiles_update (struct tiles * self) {
+    float a = self->view.x + 0.1;
+    float b = self->ncols * self->tile_width - self->view.w;
+    self->view.x = MIN(a, b);
+}
