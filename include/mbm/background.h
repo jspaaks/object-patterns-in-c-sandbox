1bcef9c6937382439cd0a8785f5c7731da29fa9e
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-12-05 14:57:39 -0500

    added timings to interface of background_update

--- include/mbm/background.h
+++ include/mbm/background.h
@@ -1,6 +1,7 @@
 #ifndef MBM_BACKGROUND_H_INCLUDED
 #define MBM_BACKGROUND_H_INCLUDED
 #include "mbm/abi.h"
+#include "mbm/timings.h"          // struct timings and associated functions
 #include "SDL3/SDL_render.h"      // SDL_Renderer
 
 // `struct background` is an opaque data structure
@@ -10,6 +11,6 @@ MBM_ABI void background_delete (struct background ** self);
 MBM_ABI void background_draw (const struct background * self, SDL_Renderer * renderer);
 MBM_ABI void background_init (struct background * self);
 MBM_ABI struct background * background_new (void);
-MBM_ABI void background_update (const struct background * self);
+MBM_ABI void background_update (struct background * self, const struct timings * timings);
 
 #endif
