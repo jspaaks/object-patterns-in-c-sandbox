c1ba4eb1acabb5cb347203e96ec2d48337653a0a
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-12-09 15:30:07 -0500

    removed animations from abi

--- src/mbm/animations.h
+++ src/mbm/animations.h
@@ -7,14 +7,14 @@
 // `struct animations` is an opaque data structure
 struct animations;
 
-MBM_ABI void animations_delete (struct animations ** self);
-MBM_ABI struct animations * animations_new (int nanims_cap, int nframes_cap, const char * relpath, SDL_Renderer * renderer);
-MBM_ABI void animations_append_anim (struct animations * self);
-MBM_ABI void animations_append_frame (struct animations * self, uint64_t duration, SDL_FRect src);
-MBM_ABI uint64_t animations_get_animation_duration (const struct animations * self, int ianim);
-MBM_ABI int animations_get_animation_nframes (const struct animations * self, int ianim);
-MBM_ABI uint64_t animations_get_frame_duration (const struct animations * self, int ianim, int iframe);
-MBM_ABI SDL_FRect animations_get_frame_src (const struct animations * self, int ianim, int iframe);
-MBM_ABI SDL_Texture * animations_get_texture (const struct animations * self);
+MBM_NO_ABI void animations_delete (struct animations ** self);
+MBM_NO_ABI struct animations * animations_new (int nanims_cap, int nframes_cap, const char * relpath, SDL_Renderer * renderer);
+MBM_NO_ABI void animations_append_anim (struct animations * self);
+MBM_NO_ABI void animations_append_frame (struct animations * self, uint64_t duration, SDL_FRect src);
+MBM_NO_ABI uint64_t animations_get_animation_duration (const struct animations * self, int ianim);
+MBM_NO_ABI int animations_get_animation_nframes (const struct animations * self, int ianim);
+MBM_NO_ABI uint64_t animations_get_frame_duration (const struct animations * self, int ianim, int iframe);
+MBM_NO_ABI SDL_FRect animations_get_frame_src (const struct animations * self, int ianim, int iframe);
+MBM_NO_ABI SDL_Texture * animations_get_texture (const struct animations * self);
 
 #endif
